38fe6d95591f1dcb6924251bd64321a90335877d
helloredreyes <hello.silentlymine@gmail.com>
2025-05-16 17:13:30 +0700

    Linted, formatted, and implemented product details functionality

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,13 @@
-import { setLocalStorage } from "./utils.mjs";
+import { getParam } from "./utils.mjs";
 import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
 const dataSource = new ProductData("tents");
+const productID = getParam("product");
 
-function addProductToCart(product) {
-  setLocalStorage("so-cart", product);
-}
+const product = new ProductDetails(productID, dataSource);
+product.init();
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
