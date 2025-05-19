4bc09211fe876cbd83642378a34e6753f0c60f5a
helloredreyes <hello.silentlymine@gmail.com>
2025-05-20 01:36:03 +0700

    change to mjs

--- vite.config.mjs
+++ vite.config.mjs
@@ -1,9 +1,19 @@
 import { resolve } from "path";
 import { defineConfig } from "vite";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
 export default defineConfig({
   root: "src/",
-
+  plugins: [
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'json',
+          dest: '.'
+        }
+      ]
+    })
+  ],
   build: {
     outDir: "../dist",
     rollupOptions: {
