f4cd37b207118a5523204d03b653a1940261480d
helloredreyes <hello.silentlymine@gmail.com>
2025-05-20 16:34:56 +0700

    resolve conflict in cart.js

--- src/js/cart.js
+++ src/js/cart.js
@@ -33,21 +33,21 @@ function cartItemTemplate(item) {
     priceHtml = formatPrice(item.FinalPrice);
   }
   const newItem = `
-  <li class="cart-card divider">
-    <span class="remove-from-cart" data-id="${item.Id}" style="cursor:pointer; color:red; float:right; font-weight:bold;">&times;</span>
-    <a href="#" class="cart-card__image">
-      <img
-        src="${item.Image}"
-        alt="${item.Name}"
-      />
-    </a>
-    <a href="#">
-      <h2 class="card__name">${item.Name}</h2>
-    </a>
-    <p class="cart-card__color">${item.Colors[0].ColorName}</p>
-    <p class="cart-card__quantity">qty: 1</p>
-    <p class="cart-card__price">${priceHtml}</p>
-  </li>`;
+    <li class="cart-card divider">
+      <span class="remove-from-cart" data-id="${item.Id}" style="cursor:pointer; color:red; float:right; font-weight:bold;">&times;</span>
+      <a href="#" class="cart-card__image">
+        <img
+          src="${item.Image}"
+          alt="${item.Name}"
+        />
+      </a>
+      <a href="#">
+        <h2 class="card__name">${item.Name}</h2>
+      </a>
+      <p class="cart-card__color">${item.Colors[0].ColorName}</p>
+      <p class="cart-card__quantity">qty: 1</p>
+      <p class="cart-card__price">${priceHtml}</p>
+    </li>`;
 
   return newItem;
 }
